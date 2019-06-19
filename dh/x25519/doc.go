15fcbbfad6f0ac8c7babb3e6908841c1c0920852
armfazh <armfazh@cloudflare.com>
2019-06-19 12:36:10 -0700

    Updating validation of key documentation.
    No changes in code, only documentation.

--- dh/x25519/doc.go
+++ dh/x25519/doc.go
@@ -3,4 +3,13 @@
 // References:
 //  - RFC7748 https://rfc-editor.org/rfc/rfc7748.txt
 //  - Curve25519 https://cr.yp.to/ecdh.html
+//
+// Validation of public keys.
+//
+// The Diffie-Hellman function, as described in RFC-7748, works for any public
+// key. However, if a different protocol requires contributory behaviour, then
+// the public keys must be validated against low-order points. To do that, the
+// Shared function performs this validation internally and returns false when
+// the public key is invalid (i.e., it is a low-order point).
+// See https://cr.yp.to/ecdh.html#validate.
 package x25519
--- dh/x448/doc.go
+++ dh/x448/doc.go
@@ -3,4 +3,13 @@
 // References:
 //  - Curve448 and Goldilocks https://eprint.iacr.org/2015/625
 //  - RFC7748 https://rfc-editor.org/rfc/rfc7748.txt
+//
+// Validation of public keys.
+//
+// The Diffie-Hellman function, as described in RFC-7748, works for any public
+// key. However, if a different protocol requires contributory behaviour, then
+// the public keys must be validated against low-order points. To do that, the
+// Shared function performs this validation internally and returns false when
+// the public key is invalid (i.e., it is a low-order point).
+// See https://cr.yp.to/ecdh.html#validate.
 package x448
