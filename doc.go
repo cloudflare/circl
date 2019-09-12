65c9a6b3eee9855a67ad73885ae3f6bad79c10b0
Kris Kwiatkowski <kris@cloudflare.com>
2019-09-12 10:12:54 +0100

    Fix small typo (from armfazh)

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Module circl provides collection of cryptographic primitives. The goal
+// Package circl provides a collection of cryptographic primitives. The goal
 // of this module is to be used as a tool for experimental deployment of
 // cryptographic algorithms targeting Post-Quantum (PQ) and Elliptic
 // Curve Cryptography (ECC).
