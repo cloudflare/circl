e3bca1717fb53a36d3b5c5d7eb1935f5be507c5e
armfazh <armfazh@cloudflare.com>
2019-12-03 10:36:54 -0800

    Fixes incomplete modular reduction.

--- math/fp448/fp_amd64.h
+++ math/fp448/fp_amd64.h
@@ -392,165 +392,144 @@
     ;;;;;;;;;;;;;; ADCXQ R8, R14; MOVQ R14, 104+z;
 
 // reduceFromDoubleLeg finds a z=x modulo p such that z<2^448 and stores in z
-// Uses: AX, DX, R8-R15, FLAGS
+// Uses: AX, R8-R15, FLAGS
 // Instr: x86_64
 #define reduceFromDoubleLeg(z,x) \
-    /* (2C13, 2C12, 2C11, 2C10|C10, C9, C8, C7) + (C6,...,C0) */ \
-    MOVQ 80+x, R10; \
+    /* (   ,2C13,2C12,2C11,2C10|C10,C9,C8, C7) + (C6,...,C0) */ \
+    /* (r14, r13, r12, r11,     r10,r9,r8,r15) */ \
+    MOVQ 80+x,AX; MOVQ AX,R10; \
+    MOVQ $0xFFFFFFFF00000000, R8; \
+    ANDQ R8,R10; \
+    \
+    MOVQ $0,R14; \
+    MOVQ 104+x,R13; SHLQ $1,R13,R14; \
+    MOVQ  96+x,R12; SHLQ $1,R12,R13; \
+    MOVQ  88+x,R11; SHLQ $1,R11,R12; \
+    MOVQ  72+x, R9; SHLQ $1,R10,R11; \
+    MOVQ  64+x, R8; SHLQ $1,R10; \
+    MOVQ $0xFFFFFFFF,R15; ANDQ R15,AX; ORQ AX,R10; \
+    MOVQ  56+x,R15; \
+    \
+    ADDQ  0+x,R15; MOVQ R15, 0+z; MOVQ  56+x,R15; \
+    ADCQ  8+x, R8; MOVQ  R8, 8+z; MOVQ  64+x, R8; \
+    ADCQ 16+x, R9; MOVQ  R9,16+z; MOVQ  72+x, R9; \
+    ADCQ 24+x,R10; MOVQ R10,24+z; MOVQ  80+x,R10; \
+    ADCQ 32+x,R11; MOVQ R11,32+z; MOVQ  88+x,R11; \
+    ADCQ 40+x,R12; MOVQ R12,40+z; MOVQ  96+x,R12; \
+    ADCQ 48+x,R13; MOVQ R13,48+z; MOVQ 104+x,R13; \
+    ADCQ   $0,R14; \
+    /* (c10c9,c9c8,c8c7,c7c13,c13c12,c12c11,c11c10) + (c6,...,c0) */ \
+    /* (   r9,  r8, r15,  r13,   r12,   r11,   r10) */ \
     MOVQ R10, AX; \
-    MOVQ $0xFFFFFFFF00000000, R15; \
-    ANDQ R15, AX; \
+    SHRQ $32,R11,R10; \
+    SHRQ $32,R12,R11; \
+    SHRQ $32,R13,R12; \
+    SHRQ $32,R15,R13; \
+    SHRQ $32, R8,R15; \
+    SHRQ $32, R9, R8; \
+    SHRQ $32, AX, R9; \
     \
-    XORL R14, R14; \
-    MOVQ 104+x, R13;  SHLQ $1, R13, R14; \
-    MOVQ  96+x, R12;  SHLQ $1, R12, R13; \
-    MOVQ  88+x, R11;  SHLQ $1, R11, R12; \
-    ;;;;;;;;;;;;;;;;  SHLQ $1,  AX, R11; \
-    ;;;;;;;;;;;;;;;;  SHLQ $1,  AX; \
-    \
-    MOVQ $0xFFFFFFFF, R15; \
-    ANDQ R15, R10; \
-    ORQ   AX, R10; \
-    \
-    MOVQ 56+x,R15; \
-    MOVQ 64+x, R8; \
-    MOVQ 72+x, R9; \
-    \
-    XORL DX, DX; \
-    ADDQ  0+x, R15;  MOVQ R15,  0+z; \
-    ADCQ  8+x,  R8;  MOVQ  R8,  8+z; \
-    ADCQ 16+x,  R9;  MOVQ  R9, 16+z; \
-    ADCQ 24+x, R10;  MOVQ R10, 24+z; \
-    ADCQ 32+x, R11;  MOVQ R11, 32+z; \
-    ADCQ 40+x, R12;  MOVQ R12, 40+z; \
-    ADCQ 48+x, R13;  MOVQ R13, 48+z; \
-    ADCQ   DX, R14;     \
-    \
-    /* (C10C9, C9C8,C8C7,C7C13,C13C12,C12C11,C11C10) + (C6,...,C0) */ \
-    MOVQ  80+x, R10;  MOVQ R10,  AX; \
-    MOVQ  88+x, R11;  SHRQ $32, R11, R10; \
-    MOVQ  96+x, R12;  SHRQ $32, R12, R11; \
-    MOVQ 104+x, R13;  SHRQ $32, R13, R12; \
-    MOVQ  56+x, R15;  SHRQ $32, R15, R13; \
-    MOVQ  64+x,  R8;  SHRQ $32,  R8, R15; \
-    MOVQ  72+x,  R9;  SHRQ $32,  R9,  R8; \
-    ;;;;;;;;;;;;;;;;  SHRQ $32,  AX,  R9; \
-    \
-    XORL DX, DX; \
-    ADDQ  0+z, R10;      MOVQ R10,  0+z; \
-    ADCQ  8+z, R11;      MOVQ R11,  8+z; \
-    ADCQ 16+z, R12;      MOVQ R12, 16+z; \
-    ADCQ 24+z, R13;      MOVQ R13, 24+z; \
-    ADCQ 32+z, R15;      MOVQ R15, 32+z; \
-    ADCQ 40+z,  R8;      MOVQ  R8, 40+z; \
-    ADCQ 48+z,  R9;      MOVQ  R9, 48+z; \
-    ADCQ  R14,  DX; \
-    \
-    /* (C7 MOD P) + (C6,...,C0) */  \
-    MOVQ DX, R15; \
-    XORL AX,  AX; \
-    MOVQ AX,  R8; \
-    MOVQ AX,  R9; \
-    MOVQ AX, R11; \
-    MOVQ AX, R12; \
-    MOVQ AX, R13; \
-    MOVQ AX, R14; \
-    MOVQ DX, R10; \
-    SHLQ $32,R10; \
-    \
-    ADDQ  0+z, R15; \
-    ADCQ  8+z,  R8;   MOVQ  R8,  8+z; \
-    ADCQ 16+z,  R9;   MOVQ  R9, 16+z; \
-    ADCQ 24+z, R10; \
-    ADCQ 32+z, R11;   MOVQ R11, 32+z; \
-    ADCQ 40+z, R12;   MOVQ R12, 40+z; \
-    ADCQ 48+z, R13;   MOVQ R13, 48+z; \
-    ADCQ  R14, R14; \
-    \
-    ADDQ R14, R15;   MOVQ R15, 0+z; \
-    SHLQ $32, R14; \
-    ADDQ R14, R10;   MOVQ R10,24+z;
+    ADDQ  0+z,R10; \
+    ADCQ  8+z,R11; \
+    ADCQ 16+z,R12; \
+    ADCQ 24+z,R13; \
+    ADCQ 32+z,R15; \
+    ADCQ 40+z, R8; \
+    ADCQ 48+z, R9; \
+    ADCQ   $0,R14; \
+    /* ( c7) + (c6,...,c0) */ \
+    /* (r14) */ \
+    MOVQ R14, AX; SHLQ $32, AX; \
+    ADDQ R14,R10; MOVQ  $0,R14; \
+    ADCQ  $0,R11; \
+    ADCQ  $0,R12; \
+    ADCQ  AX,R13; \
+    ADCQ  $0,R15; \
+    ADCQ  $0, R8; \
+    ADCQ  $0, R9; \
+    ADCQ  $0,R14; \
+    /* ( c7) + (c6,...,c0) */ \
+    /* (r14) */ \
+    MOVQ R14, AX; SHLQ $32,AX; \
+    ADDQ R14,R10; MOVQ R10, 0+z; \
+    ADCQ  $0,R11; MOVQ R11, 8+z; \
+    ADCQ  $0,R12; MOVQ R12,16+z; \
+    ADCQ  AX,R13; MOVQ R13,24+z; \
+    ADCQ  $0,R15; MOVQ R15,32+z; \
+    ADCQ  $0, R8; MOVQ  R8,40+z; \
+    ADCQ  $0, R9; MOVQ  R9,48+z;
 
 // reduceFromDoubleAdx finds a z=x modulo p such that z<2^448 and stores in z
-// Uses: AX, DX, R8-R15, FLAGS
+// Uses: AX, R8-R15, FLAGS
 // Instr: x86_64, adx
 #define reduceFromDoubleAdx(z,x) \
-    /* (2C13, 2C12, 2C11, 2C10|C10, C9, C8, C7) + (C6,...,C0) */ \
-    MOVQ 80+x, R10; \
+    /* (   ,2C13,2C12,2C11,2C10|C10,C9,C8, C7) + (C6,...,C0) */ \
+    /* (r14, r13, r12, r11,     r10,r9,r8,r15) */ \
+    MOVQ 80+x,AX; MOVQ AX,R10; \
+    MOVQ $0xFFFFFFFF00000000, R8; \
+    ANDQ R8,R10; \
+    \
+    MOVQ $0,R14; \
+    MOVQ 104+x,R13; SHLQ $1,R13,R14; \
+    MOVQ  96+x,R12; SHLQ $1,R12,R13; \
+    MOVQ  88+x,R11; SHLQ $1,R11,R12; \
+    MOVQ  72+x, R9; SHLQ $1,R10,R11; \
+    MOVQ  64+x, R8; SHLQ $1,R10; \
+    MOVQ $0xFFFFFFFF,R15; ANDQ R15,AX; ORQ AX,R10; \
+    MOVQ  56+x,R15; \
+    \
+    XORL AX,AX; \
+    ADCXQ  0+x,R15; MOVQ R15, 0+z; MOVQ  56+x,R15; \
+    ADCXQ  8+x, R8; MOVQ  R8, 8+z; MOVQ  64+x, R8; \
+    ADCXQ 16+x, R9; MOVQ  R9,16+z; MOVQ  72+x, R9; \
+    ADCXQ 24+x,R10; MOVQ R10,24+z; MOVQ  80+x,R10; \
+    ADCXQ 32+x,R11; MOVQ R11,32+z; MOVQ  88+x,R11; \
+    ADCXQ 40+x,R12; MOVQ R12,40+z; MOVQ  96+x,R12; \
+    ADCXQ 48+x,R13; MOVQ R13,48+z; MOVQ 104+x,R13; \
+    ADCXQ   AX,R14; \
+    /* (c10c9,c9c8,c8c7,c7c13,c13c12,c12c11,c11c10) + (c6,...,c0) */ \
+    /* (   r9,  r8, r15,  r13,   r12,   r11,   r10) */ \
     MOVQ R10, AX; \
-    MOVQ $0xFFFFFFFF00000000, R15; \
-    ANDQ R15, AX; \
+    SHRQ $32,R11,R10; \
+    SHRQ $32,R12,R11; \
+    SHRQ $32,R13,R12; \
+    SHRQ $32,R15,R13; \
+    SHRQ $32, R8,R15; \
+    SHRQ $32, R9, R8; \
+    SHRQ $32, AX, R9; \
     \
-    XORL R14, R14; \
-    MOVQ 104+x, R13;  SHLQ $1, R13, R14; \
-    MOVQ  96+x, R12;  SHLQ $1, R12, R13; \
-    MOVQ  88+x, R11;  SHLQ $1, R11, R12; \
-    ;;;;;;;;;;;;;;;;  SHLQ $1,  AX, R11; \
-    ;;;;;;;;;;;;;;;;  SHLQ $1,  AX; \
-    \
-    MOVQ $0xFFFFFFFF, R15; \
-    ANDQ R15, R10; \
-    ORQ   AX, R10; \
-    \
-    MOVQ 56+x,R15; \
-    MOVQ 64+x, R8; \
-    MOVQ 72+x, R9; \
-    \
-    XORL DX, DX; \
-    ADCXQ  0+x, R15;  MOVQ R15,  0+z; \
-    ADCXQ  8+x,  R8;  MOVQ  R8,  8+z; \
-    ADCXQ 16+x,  R9;  MOVQ  R9, 16+z; \
-    ADCXQ 24+x, R10;  MOVQ R10, 24+z; \
-    ADCXQ 32+x, R11;  MOVQ R11, 32+z; \
-    ADCXQ 40+x, R12;  MOVQ R12, 40+z; \
-    ADCXQ 48+x, R13;  MOVQ R13, 48+z; \
-    ADCXQ   DX, R14; \
-    \
-    /* (C10C9, C9C8,C8C7,C7C13,C13C12,C12C11,C11C10) + (C6,...,C0) */ \
-    MOVQ  80+x, R10;  MOVQ R10, AX; \
-    MOVQ  88+x, R11;  SHRQ $32, R11, R10; \
-    MOVQ  96+x, R12;  SHRQ $32, R12, R11; \
-    MOVQ 104+x, R13;  SHRQ $32, R13, R12; \
-    MOVQ  56+x, R15;  SHRQ $32, R15, R13; \
-    MOVQ  64+x,  R8;  SHRQ $32,  R8, R15; \
-    MOVQ  72+x,  R9;  SHRQ $32,  R9,  R8; \
-    ;;;;;;;;;;;;;;;;  SHRQ $32,  AX,  R9; \
-    \
-    XORL DX, DX; \
-    ADCXQ  0+z, R10;  MOVQ R10,  0+z; \
-    ADCXQ  8+z, R11;  MOVQ R11,  8+z; \
-    ADCXQ 16+z, R12;  MOVQ R12, 16+z; \
-    ADCXQ 24+z, R13;  MOVQ R13, 24+z; \
-    ADCXQ 32+z, R15;  MOVQ R15, 32+z; \
-    ADCXQ 40+z,  R8;  MOVQ  R8, 40+z; \
-    ADCXQ 48+z,  R9;  MOVQ  R9, 48+z; \
-    ADCXQ  R14,  DX; \
-    \
-    /* (C7 MOD P) + (C6,...,C0) */ \
-    MOVQ DX, R15; \
-    XORL AX, AX; \
-    MOVQ AX,  R8; \
-    MOVQ AX,  R9; \
-    MOVQ AX, R11; \
-    MOVQ AX, R12; \
-    MOVQ AX, R13; \
-    MOVQ AX, R14; \
-    MOVQ DX, R10; \
-    SHLQ $32,R10; \
-    \
+    XORL AX,AX; \
+    ADCXQ  0+z,R10; \
+    ADCXQ  8+z,R11; \
+    ADCXQ 16+z,R12; \
+    ADCXQ 24+z,R13; \
+    ADCXQ 32+z,R15; \
+    ADCXQ 40+z, R8; \
+    ADCXQ 48+z, R9; \
+    ADCXQ   AX,R14; \
+    /* ( c7) + (c6,...,c0) */ \
+    /* (r14) */ \
+    MOVQ R14, AX; SHLQ $32, AX; \
     CLC; \
-    ADCXQ  0+z,R15; \
-    ADCXQ  8+z, R8;   MOVQ  R8,  8+z; \
-    ADCXQ 16+z, R9;   MOVQ  R9, 16+z; \
-    ADCXQ 24+z,R10; \
-    ADCXQ 32+z,R11;   MOVQ R11, 32+z; \
-    ADCXQ 40+z,R12;   MOVQ R12, 40+z; \
-    ADCXQ 48+z,R13;   MOVQ R13, 48+z; \
-    ADCXQ  R14,R14; \
-    \
-    ADDQ R14,R15;   MOVQ R15, 0+z; \
-    SHLQ $32,R14; \
-    ADDQ R14,R10;   MOVQ R10,24+z;
+    ADCXQ R14,R10; MOVQ $0,R14; \
+    ADCXQ R14,R11; \
+    ADCXQ R14,R12; \
+    ADCXQ  AX,R13; \
+    ADCXQ R14,R15; \
+    ADCXQ R14, R8; \
+    ADCXQ R14, R9; \
+    ADCXQ R14,R14; \
+    /* ( c7) + (c6,...,c0) */ \
+    /* (r14) */ \
+    MOVQ R14, AX; SHLQ $32, AX; \
+    CLC; \
+    ADCXQ R14,R10; MOVQ R10, 0+z; MOVQ $0,R14; \
+    ADCXQ R14,R11; MOVQ R11, 8+z; \
+    ADCXQ R14,R12; MOVQ R12,16+z; \
+    ADCXQ  AX,R13; MOVQ R13,24+z; \
+    ADCXQ R14,R15; MOVQ R15,32+z; \
+    ADCXQ R14, R8; MOVQ  R8,40+z; \
+    ADCXQ R14, R9; MOVQ  R9,48+z;
 
 // addSub calculates two operations: x,y = x+y,x-y
 // Uses: AX, DX, R8-R15, FLAGS
