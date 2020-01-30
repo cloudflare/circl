0549cdfbf2f6f72af971ea5c8a8b25f9b3500935
armfazh <armfazh@cloudflare.com>
2020-01-30 15:49:46 -0800

    Removing unused point generator.

--- sign/ed25519/internal_test.go
+++ sign/ed25519/internal_test.go
@@ -85,7 +85,7 @@ func TestRangeOrder(t *testing.T) {
 	}
 
 	for i := range aboveOrder {
-		got := isLessThan(aboveOrder[i][:], curve.order[:])
+		got := isLessThan(aboveOrder[i][:], order[:])
 		want := false
 		if got != want {
 			test.ReportError(t, got, want, i, aboveOrder[i])
