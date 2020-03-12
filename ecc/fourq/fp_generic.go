62986c435778e423fc2eda96e7948ce6508f2f40
armfazh <armfazh@cloudflare.com>
2020-03-11 20:43:14 -0700

    Calling generic from Fq

--- ecc/fourq/fp_generic.go
+++ ecc/fourq/fp_generic.go
@@ -17,20 +17,18 @@ func fpAddGeneric(c, a, b *Fp) {
 	aa, bb, cc := (*elt64)(unsafe.Pointer(a)), (*elt64)(unsafe.Pointer(b)), (*elt64)(unsafe.Pointer(c))
 	c0, x := bits.Add64(aa[0], bb[0], 0)
 	c1, _ := bits.Add64(aa[1], bb[1], x)
-	bit := (c1 >> 63) & 1
-	c1 = c1 &^ mask
-	cc[0], x = bits.Add64(c0, 0, bit)
-	cc[1], _ = bits.Add64(c1, 0, x)
+	c1, x = bits.Add64(c1, c1, 0)
+	cc[0], x = bits.Add64(c0, 0, x)
+	cc[1], _ = bits.Add64(c1>>1, 0, x)
 }
 
 func fpSubGeneric(c, a, b *Fp) {
 	aa, bb, cc := (*elt64)(unsafe.Pointer(a)), (*elt64)(unsafe.Pointer(b)), (*elt64)(unsafe.Pointer(c))
 	c0, x := bits.Sub64(aa[0], bb[0], 0)
 	c1, _ := bits.Sub64(aa[1], bb[1], x)
-	x = (c1 & mask) >> 63
-	c1 = c1 &^ mask
+	c1, x = bits.Add64(c1, c1, 0)
 	cc[0], x = bits.Sub64(c0, 0, x)
-	cc[1], _ = bits.Sub64(c1, 0, x)
+	cc[1], _ = bits.Sub64(c1>>1, 0, x)
 }
 
 func fpMulGeneric(c, a, b *Fp) {
@@ -38,7 +36,8 @@ func fpMulGeneric(c, a, b *Fp) {
 	c1, c0 := bits.Mul64(aa[0], bb[0])
 	hi, lo := bits.Mul64(aa[0], bb[1])
 	c0, x := bits.Add64(c0, hi<<1, 0)
-	c1, c2 := bits.Add64(c1, lo, x)
+	c1, x = bits.Add64(c1, lo, x)
+	c2, _ := bits.Add64(0, 0, x)
 
 	hi, lo = bits.Mul64(aa[1], bb[0])
 	c0, x = bits.Add64(c0, hi<<1, 0)
@@ -46,22 +45,20 @@ func fpMulGeneric(c, a, b *Fp) {
 	c2, _ = bits.Add64(c2, 0, x)
 
 	hi, lo = bits.Mul64(aa[1], bb[1])
-	hi = (hi << 1) | (lo >> 63)
-	lo = lo << 1
+	lo, x = bits.Add64(lo, lo, 0)
+	hi, _ = bits.Add64(hi, hi, x)
+
 	c0, x = bits.Add64(c0, lo, 0)
 	c1, x = bits.Add64(c1, hi, x)
 	c2, _ = bits.Add64(c2, 0, x)
 
-	c2 = c2 << 1
-	x = (c1 & mask) >> 63
-	c1 = c1 &^ mask
-	c0, x = bits.Add64(c0, c2, x)
-	c1, _ = bits.Add64(c1, 0, x)
+	c1, x = bits.Add64(c1, c1, 0)
+	c0, x = bits.Add64(c0, c2<<1, x)
+	c1, _ = bits.Add64(c1>>1, 0, x)
 
-	x = (c1 & mask) >> 63
-	c1 = c1 &^ mask
+	c1, x = bits.Add64(c1, c1, 0)
 	cc[0], x = bits.Add64(c0, 0, x)
-	cc[1], _ = bits.Add64(c1, 0, x)
+	cc[1], _ = bits.Add64(c1>>1, 0, x)
 }
 
 func fpSqrGeneric(c, a *Fp) { fpMulGeneric(c, a, a) }
