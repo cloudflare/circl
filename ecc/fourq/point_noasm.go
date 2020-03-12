61eeda9a1734b7d1305feeee15f3bf80f886a816
armfazh <armfazh@cloudflare.com>
2020-03-11 17:32:45 -0700

    FourQ runs in any arch with generic code.

---
+++ ecc/fourq/point_noasm.go
@@ -0,0 +1,7 @@
+// +build !amd64 purego
+
+package fourq
+
+func (P *pointR1) double()           { doubleGeneric(P) }
+func (P *pointR1) add(Q *pointR2)    { addGeneric(P, Q) }
+func (P *pointR1) mixAdd(Q *pointR3) { mixAddGeneric(P, Q) }
--- ecc/fourq/point_test.go
+++ ecc/fourq/point_test.go
@@ -1,5 +1,3 @@
-// +build amd64,go1.12
-
 package fourq
 
 import (
