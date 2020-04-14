a1bb5e558e66888d1c8e00799e8cd6f945207171
armfazh <armfazh@cloudflare.com>
2020-04-13 17:44:51 -0700

    Improving with suggestions made in review.

--- sign/ed448/ed448.go
+++ sign/ed448/ed448.go
@@ -1,4 +1,4 @@
-// Package ed448 provides the signature scheme Ed448 as described in RFC-8032.
+// Package ed448 implements Ed448 signature scheme as described in RFC-8032.
 //
 // References:
 //  - RFC8032 https://rfc-editor.org/rfc/rfc8032.txt
@@ -9,8 +9,9 @@ package ed448
 import (
 	"bytes"
 	"crypto"
-	"crypto/rand"
+	cryptoRand "crypto/rand"
 	"errors"
+	"fmt"
 	"io"
 
 	"github.com/cloudflare/circl/ecc/goldilocks"
@@ -22,6 +23,8 @@ const (
 	Size = 57
 	// SignatureSize is the length in bytes of signatures.
 	SignatureSize = 2 * Size
+	// MaxContextLength is the maximum length in bytes of context strings.
+	MaxContextLength = 255
 )
 
 // PublicKey represents a public key of Ed448.
@@ -45,11 +48,10 @@ func (k *KeyPair) Seed() []byte { return k.GetPrivate() }
 // Public returns a crypto.PublicKey corresponding to the private key.
 func (k *KeyPair) Public() crypto.PublicKey { return k.GetPublic() }
 
-// Sign signs the given message with priv.
-// Ed448 performs two passes over messages to be signed and therefore cannot
-// handle pre-hashed messages. Thus opts.HashFunc() must return zero to
-// indicate the message hasn't been hashed. This can be achieved by passing
-// crypto.Hash(0) as the value for opts.
+// Sign creates a signature of a message given a key pair.
+// The opts.HashFunc() must return zero to indicate the message hasn't been
+// hashed. This can be achieved by passing crypto.Hash(0) as the value for opts.
+// This function signs using as context the empty string.
 func (k *KeyPair) Sign(rand io.Reader, message []byte, opts crypto.SignerOpts) ([]byte, error) {
 	if opts.HashFunc() != crypto.Hash(0) {
 		return nil, errors.New("ed448: cannot sign hashed message")
@@ -57,21 +59,20 @@ func (k *KeyPair) Sign(rand io.Reader, message []byte, opts crypto.SignerOpts) (
 	return Sign(k, message, nil), nil
 }
 
-// GenerateKey generates a public and private key pair using entropy from rand.
+// GenerateKey produces a pair of public and private keys using entropy from rand.
 // If rand is nil, crypto/rand.Reader will be used.
-func GenerateKey(rnd io.Reader) (*KeyPair, error) {
-	if rnd == nil {
-		rnd = rand.Reader
+func GenerateKey(rand io.Reader) (*KeyPair, error) {
+	if rand == nil {
+		rand = cryptoRand.Reader
 	}
 	seed := make(PrivateKey, Size)
-	if _, err := io.ReadFull(rnd, seed); err != nil {
+	if _, err := io.ReadFull(rand, seed); err != nil {
 		return nil, err
 	}
 	return NewKeyFromSeed(seed), nil
 }
 
-// NewKeyFromSeed generates a pair of Ed448 signing keys given a
-// previously-generated private key.
+// NewKeyFromSeed generates a pair of Ed448 keys given a private key.
 func NewKeyFromSeed(seed PrivateKey) *KeyPair {
 	if len(seed) != Size {
 		panic("ed448: bad private key length")
@@ -80,88 +81,112 @@ func NewKeyFromSeed(seed PrivateKey) *KeyPair {
 	H := sha3.NewShake256()
 	_, _ = H.Write(seed)
 	_, _ = H.Read(h[:])
-	clamp(h[:Size])
+	s := &goldilocks.Scalar{}
+	deriveSecretScalar(s, h[:Size])
 
-	var _r goldilocks.Scalar
-	_r.FromBytes(h[:Size])
-	pk := &KeyPair{}
-	goldilocks.Curve{}.ScalarBaseMult(&_r).ToBytes(pk.public[:])
-	copy(pk.private[:], seed)
-	return pk
+	pair := &KeyPair{}
+	copy(pair.private[:], seed)
+	goldilocks.Curve{}.ScalarBaseMult(s).ToBytes(pair.public[:])
+	return pair
 }
 
-// Sign returns the signature of a message using both the private and public
-// keys of the signer.
+// Sign creates a signature of a message given a key pair. The context is a
+// constant string that separates uses of the signature between different protocols.
+// See Section 8.3 of RFC-8032 (https://tools.ietf.org/html/rfc8032#section-8.3).
 func Sign(kp *KeyPair, message, context []byte) []byte {
-	if len(context) > 255 {
-		panic("context should be at most 255 octets")
+	if len(context) > MaxContextLength {
+		panic(fmt.Sprintf("context should be at most %v bytes", MaxContextLength))
 	}
-	var r, h, hRAM [2 * Size]byte
+	// 1.  Hash the 57-byte private key using SHAKE256(x, 114).
+	var h [2 * Size]byte
 	H := sha3.NewShake256()
 	_, _ = H.Write(kp.private[:])
 	_, _ = H.Read(h[:])
+	s := &goldilocks.Scalar{}
+	deriveSecretScalar(s, h[:Size])
+	prefix := h[Size:]
 
-	prefix := [10]byte{'S', 'i', 'g', 'E', 'd', '4', '4', '8', byte(0), byte(len(context))}
+	// 2.  Compute SHAKE256(dom4(F, C) || prefix || PH(M), 114).
+	var rPM [2 * Size]byte
+	dom4 := [10]byte{'S', 'i', 'g', 'E', 'd', '4', '4', '8', byte(0), byte(len(context))}
 	H.Reset()
-	_, _ = H.Write(prefix[:])
+	_, _ = H.Write(dom4[:])
 	_, _ = H.Write(context)
-	_, _ = H.Write(h[Size:])
+	_, _ = H.Write(prefix)
 	_, _ = H.Write(message)
-	_, _ = H.Read(r[:])
+	_, _ = H.Read(rPM[:])
 
-	var _r goldilocks.Scalar
-	_r.FromBytes(r[:])
-	signature := make([]byte, 2*Size)
-	goldilocks.Curve{}.ScalarBaseMult(&_r).ToBytes(signature[:Size])
+	// 3.  Compute the point [r]B.
+	r := &goldilocks.Scalar{}
+	r.FromBytes(rPM[:])
+	R := (&[Size]byte{})[:]
+	goldilocks.Curve{}.ScalarBaseMult(r).ToBytes(R)
 
+	// 4.  Compute SHAKE256(dom4(F, C) || R || A || PH(M), 114)
+	var hRAM [2 * Size]byte
 	H.Reset()
-	_, _ = H.Write(prefix[:])
+	_, _ = H.Write(dom4[:])
 	_, _ = H.Write(context)
-	_, _ = H.Write(signature[:Size])
+	_, _ = H.Write(R)
 	_, _ = H.Write(kp.public[:])
 	_, _ = H.Write(message)
 	_, _ = H.Read(hRAM[:])
 
-	var _hRAM, _h, _s goldilocks.Scalar
-	_hRAM.FromBytes(hRAM[:])
-	clamp(h[:Size])
-	_h.FromBytes(h[:Size])
-	_s.Mul(&_h, &_hRAM)
-	_s.Add(&_s, &_r)
-	copy(signature[Size:], _s[:])
-	return signature
+	// 5.  Compute S = (r + k * s) mod order.
+	k := &goldilocks.Scalar{}
+	k.FromBytes(hRAM[:])
+	S := &goldilocks.Scalar{}
+	S.Mul(k, s)
+	S.Add(S, r)
+
+	// 6.  The signature is the concatenation of R and S.
+	var signature [SignatureSize]byte
+	copy(signature[:Size], R[:])
+	copy(signature[Size:], S[:])
+	return signature[:SignatureSize]
 }
 
 // Verify returns true if the signature is valid. Failure cases are invalid
 // signature, or when the public key cannot be decoded.
 func Verify(public PublicKey, message, context, signature []byte) bool {
-	P, errDecoding := goldilocks.FromBytes(public)
 	if len(public) != Size ||
-		len(signature) != 2*Size ||
-		len(context) > 255 ||
-		!isLessThanOrder(signature[Size:]) ||
-		errDecoding != nil {
+		len(signature) != SignatureSize ||
+		len(context) > MaxContextLength ||
+		!isLessThanOrder(signature[Size:]) {
+		return false
+	}
+	P, err := goldilocks.FromBytes(public)
+	if err != nil {
 		return false
 	}
 
 	var hRAM [2 * Size]byte
-	prefix := [10]byte{'S', 'i', 'g', 'E', 'd', '4', '4', '8', byte(0), byte(len(context))}
+	dom4 := [10]byte{'S', 'i', 'g', 'E', 'd', '4', '4', '8', byte(0), byte(len(context))}
+	R := signature[:Size]
 	H := sha3.NewShake256()
-	_, _ = H.Write(prefix[:])
+	_, _ = H.Write(dom4[:])
 	_, _ = H.Write(context)
-	_, _ = H.Write(signature[:Size])
+	_, _ = H.Write(R)
 	_, _ = H.Write(public[:Size])
 	_, _ = H.Write(message)
 	_, _ = H.Read(hRAM[:])
 
-	var _s, _hRAM goldilocks.Scalar
-	_hRAM.FromBytes(hRAM[:])
-	_s.FromBytes(signature[Size:])
+	k := &goldilocks.Scalar{}
+	k.FromBytes(hRAM[:])
+	S := &goldilocks.Scalar{}
+	S.FromBytes(signature[Size:])
 
+	encR := (&[Size]byte{})[:]
 	P.Neg()
-	var enc [Size]byte
-	goldilocks.Curve{}.CombinedMult(&_s, &_hRAM, P).ToBytes(enc[:])
-	return bytes.Equal(enc[:], signature[:Size])
+	goldilocks.Curve{}.CombinedMult(S, k, P).ToBytes(encR)
+	return bytes.Equal(R, encR)
+}
+
+func deriveSecretScalar(s *goldilocks.Scalar, h []byte) {
+	h[0] &= 0xFC      // The two least significant bits of the first octet are cleared,
+	h[Size-1] = 0x00  // all eight bits the last octet are cleared, and
+	h[Size-2] |= 0x80 // the highest bit of the second to last octet is set.
+	s.FromBytes(h[:Size])
 }
 
 // isLessThanOrder returns true if 0 <= x < order.
@@ -172,16 +197,6 @@ func isLessThanOrder(x []byte) bool {
 		i--
 	}
 	return x[Size-1] == 0 && x[i] < order[i]
-}
-
-// clamp prines the buffer as indicated in rfc8032 (https://tools.ietf.org/html/rfc8032#section-5.2.5)
-// 1. The two least significant bits of the first octet are cleared,
-// 2. all eight bits the last octet are cleared, and
-// 3. the highest bit of the second to last octet is set.
-func clamp(k []byte) {
-	k[0] &= 252
-	k[Size-1] = 0x00
-	k[Size-2] |= 0x80
 }
 
 func makeCopy(in *[Size]byte) []byte {
