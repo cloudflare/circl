4f61e6c870e171f161d6bcd6d6eb910634625944
armfazh <armfazh@cloudflare.com>
2020-06-16 04:50:15 -0700

    Miller loop is working.

--- ecc/bls12381/ec2.go
+++ ecc/bls12381/ec2.go
@@ -14,23 +14,28 @@ func doubleAndLine(P *G2, l *line) {
 	isDoubLine := l != nil
 	_3B := &g2Param3B
 	var A, B, C, D, E, F, G, T ff.Fp2
-	B.Sqr(Y)       // 1.  B  = Y1^2
-	C.Sqr(Z)       // 2.  C  = Z1^2
-	D.Mul(_3B, &C) // 3.  D  = 3b*C
+	B.Sqr(Y)       // 1. B = Y1^2
+	C.Sqr(Z)       // 2. C = Z1^2
+	D.Mul(_3B, &C) // 3. D = 3b*C
+	F.Add(Y, Z)    // 4. F = (Y1+Z1)
+	F.Sqr(&F)      //    F = (Y1+Z1)^2
+	F.Sub(&F, &B)  //    F = (Y1+Z1)^2-B
+	F.Sub(&F, &C)  //    F = (Y1+Z1)^2-B-C
 	if isDoubLine {
-		A.Sqr(X)      // 4.  A  = X1^2
-		E.Add(X, Y)   //     E  = (X1+Y1)
-		E.Sqr(&E)     //     E  = (X1+Y1)^2
-		E.Sub(&E, &A) //     E  = (X1+Y1)^2-A
-		E.Sub(&E, &B) //     E  = (X1+Y1)^2-A-B
+		A.Sqr(X)            // 5.  A  = X1^2
+		E.Add(X, Y)         //     E  = (X1+Y1)
+		E.Sqr(&E)           //     E  = (X1+Y1)^2
+		E.Sub(&E, &A)       //     E  = (X1+Y1)^2-A
+		E.Sub(&E, &B)       //     E  = (X1+Y1)^2-A-B = 2X*Y
+		l[0].Add(&A, &A)    // 5a. l0 = 2A
+		l[0].Add(&l[0], &A) //     l0 = 3A = 3X1^2
+		l[1].Set(&F)        // 5b. l1 = F
+		l[1].Neg()          //     l1 = -F = -2Y1Z1
+		l[2].Sub(&D, &B)    // 5c. l2 = D-B = 3b*Z1^2-Y1^2
 	} else {
-		E.Mul(X, Y)   // 4.  E = X*Y
-		E.Add(&E, &E) //     E = 2X*Y
+		E.Mul(X, Y)   // 5. E = X*Y
+		E.Add(&E, &E) //    E = 2X*Y
 	}
-	F.Add(Y, Z)    // 5.  F  = (Y1+Z1)
-	F.Sqr(&F)      //     F  = (Y1+Z1)^2
-	F.Sub(&F, &B)  //     F  = (Y1+Z1)^2-B
-	F.Sub(&F, &C)  //     F  = (Y1+Z1)^2-B-C
 	T.Add(&D, &D)  // 6.  T  = 2D
 	G.Add(&T, &D)  // 7.  G  = 3D
 	X3.Sub(&B, &G) // 8.  X3 = (B-G)
@@ -45,13 +50,6 @@ func doubleAndLine(P *G2, l *line) {
 	Z3.Add(Z3, Z3) //     Z3 = 2B*F
 	Z3.Add(Z3, Z3) //     Z3 = 4B*F
 	P.Set(&R)
-	if isDoubLine {
-		l.l1.Add(&A, &A)    // 12. l1 = 2A
-		l.l1.Add(&l.l1, &A) //     l1 = 3A = 3X1
-		l.l0.Set(&F)        // 13. l0 = F
-		l.l0.Neg()          //     l0 = -F = -2Y1Z1
-		l.l3.Sub(&D, &B)    // 14. l3 = D-B = 3b*Z1^2-Y1^2
-	}
 }
 
 func addAndLine(PQ, P, Q *G2, l *line) {
@@ -77,8 +75,8 @@ func addAndLine(PQ, P, Q *G2, l *line) {
 
 	A.Add(&X1X2, &X1X2)    // A = 2X1X2
 	A.Add(&A, &X1X2)       //   = 3X1X2
-	B.Add(&Y1Y2, &_3bZ1Z2) // B = Y1Y2+3BZ1Z2
-	C.Sub(&Y1Y2, &_3bZ1Z2) // C = Y1Y2-3BZ1Z2
+	B.Add(&Y1Y2, &_3bZ1Z2) // B = Y1Y2+3bZ1Z2
+	C.Sub(&Y1Y2, &_3bZ1Z2) // C = Y1Y2-3bZ1Z2
 
 	t0.Add(X1, Y1)   // t0 = (X1 + Y1)
 	D.Add(X2, Y2)    // D  = (X2 + Y2)
@@ -91,19 +89,19 @@ func addAndLine(PQ, P, Q *G2, l *line) {
 		t0.Mul(Y1, Z2) // t0 = Y1Z2
 		t1.Mul(Y2, Z1) // t1 = Y2Z1
 		E.Add(t0, t1)  // E  = Y1Z2 + Y2Z1
-		EE.Sub(t1, t0) // EE = Y2Z1 - Y1Z2
+		EE.Sub(t0, t1) // EE = Y1Z2 - Y2Z1
 
 		t0.Mul(X1, Z2) // t0 = X1Z2
 		t1.Mul(X2, Z1) // t1 = X2Z1
 		F.Add(t0, t1)  // F  = X1Z2 + X2Z1
 		FF.Sub(t0, t1) // FF = X1Z2 - X2Z1
 
-		l.l0.Mul(&FF, Z2)   // l0 = (X1Z2 - X2Z1)*Z2
-		l.l1.Mul(&EE, Z2)   // l1 = (Y2Z1 - Y1Z2)*Z2
-		t0.Mul(&EE, X2)     // t0 = (Y2Z1 - Y1Z2)*X2
-		l.l3.Mul(&FF, Y2)   // l2 = (X1Z2 - X2Z1)*Y2
-		l.l3.Add(&l.l3, Y2) //    = (Y2Z1 - Y1Z2)*X2 + (X1Z2 - X2Z1)*Y2
-		l.l3.Neg()          //    = (Y1Z2 - Y2Z1)*X2 - (X1Z2 - X2Z1)*Y2
+		l[0].Mul(&EE, Z2)   // l0 = (Y1Z2 - Y2Z1)*Z2
+		l[0].Neg()          //    = -(Y1Z2 - Y2Z1)*Z2
+		l[1].Mul(&FF, Z2)   // l1 = (X1Z2 - X2Z1)*Z2
+		t0.Mul(&FF, Y2)     // t0 = (X1Z2 - X2Z1)*Y2
+		l[2].Mul(&EE, X2)   // l2 = (Y1Z2 - Y2Z1)*X2
+		l[2].Sub(&l[2], t0) //    = (Y1Z2 - Y2Z1)*X2 - (X1Z2 - X2Z1)*Y2
 	} else {
 		t0.Add(Y1, Z1)   // t0 = (Y1 + Z1)
 		t1.Add(Y2, Z2)   // t1 = (Y2 + Z2)
