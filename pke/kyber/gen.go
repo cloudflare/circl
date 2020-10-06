3add18dfea8499f9d170be2ae46f2d3d316be6a7
Bas Westerbaan <bas@westerbaan.name>
2020-10-06 13:48:14 +0200

    kyber: do not use symlinks
    Cf. #149

--- pke/kyber/gen.go
+++ pke/kyber/gen.go
@@ -6,7 +6,10 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"path"
 	"strings"
 	"text/template"
 )
@@ -56,6 +59,7 @@ var (
 func main() {
 	generatePackageFiles()
 	generateParamsFiles()
+	generateSourceFiles()
 }
 
 // Generates instance/internal/params.go from templates/params.templ.go
@@ -107,6 +111,87 @@ func generatePackageFiles() {
 		err = ioutil.WriteFile(mode.Pkg()+"/kyber.go", []byte(res[offset:]), 0644)
 		if err != nil {
 			panic(err)
+		}
+	}
+}
+
+// Copies kyber512 source files to other modes
+func generateSourceFiles() {
+	files := make(map[string][]byte)
+
+	// Ignore mode specific files.
+	ignored := func(x string) bool {
+		return x == "params.go" || x == "params_test.go"
+	}
+
+	fs, err := ioutil.ReadDir("kyber512/internal")
+	if err != nil {
+		panic(err)
+	}
+
+	// Read files
+	for _, f := range fs {
+		name := f.Name()
+		if ignored(name) {
+			continue
+		}
+		files[name], err = ioutil.ReadFile(path.Join("kyber512/internal", name))
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Go over modes
+	for _, mode := range Instances {
+		if mode.Name == "Kyber512" {
+			continue
+		}
+
+		fs, err = ioutil.ReadDir(path.Join(mode.Pkg(), "internal"))
+		for _, f := range fs {
+			name := f.Name()
+			fn := path.Join(mode.Pkg(), "internal", name)
+			if ignored(name) {
+				continue
+			}
+			_, ok := files[name]
+			if !ok {
+				fmt.Printf("Removing superfluous file: %s", fn)
+				err = os.Remove(fn)
+				if err != nil {
+					panic(err)
+				}
+			}
+			if f.Mode().IsDir() {
+				panic(fmt.Sprintf("%s: is a directory", fn))
+			}
+			if f.Mode()&os.ModeSymlink != 0 {
+				fmt.Printf("Removing symlink: %s\n", fn)
+				err = os.Remove(fn)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+		for name, expected := range files {
+			fn := path.Join(mode.Pkg(), "internal", name)
+			expected = []byte(fmt.Sprintf(
+				"%s kyber512/internal/%s by gen.go\n\n%s",
+				TemplateWarning,
+				name,
+				string(expected),
+			))
+			got, err := ioutil.ReadFile(fn)
+			if err == nil {
+				if bytes.Equal(got, expected) {
+					continue
+				}
+			}
+			fmt.Printf("Updating %s\n", fn)
+			err = ioutil.WriteFile(fn, expected, 0644)
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
 }
