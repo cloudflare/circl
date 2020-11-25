70cc1589ad20ec24daa32bb9de87add52a9320a5
Bas Westerbaan <bas@westerbaan.name>
2020-11-25 19:06:48 +0100

    simd: add two-way f1600 using SHA3 extensions for arm64
    Based on github.com/bwesterb/armed-keccak

--- simd/keccakf1600/fallback.go
+++ simd/keccakf1600/fallback.go
@@ -1,5 +1,7 @@
-// +build !amd64
+// +build !amd64,!arm64 arm64,!go1.16
 
 package keccakf1600
 
-func permuteSIMD(state []uint64) { permuteScalar(state) }
+func permuteSIMDx2(state []uint64) { permuteScalarX2(state) }
+
+func permuteSIMDx4(state []uint64) { permuteScalarX4(state) }
