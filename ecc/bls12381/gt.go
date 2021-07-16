65b299712848f6d56d91ce08b6c2054afe7d871f
armfazh <armfazh@cloudflare.com>
2021-07-16 16:18:38 -0700

    Adding methods to Gt.

--- ecc/bls12381/gt.go
+++ ecc/bls12381/gt.go
@@ -2,9 +2,14 @@ package bls12381
 
 import "github.com/cloudflare/circl/ecc/bls12381/ff"
 
+// Gt represents an element of the output group of a pairing.
 type Gt ff.Fp12
 
+func (z Gt) String() string        { return (ff.Fp12)(z).String() }
+func (z *Gt) Set(x *Gt)            { (*ff.Fp12)(z).Set((*ff.Fp12)(x)) }
 func (z *Gt) SetOne()              { (*ff.Fp12)(z).SetOne() }
 func (z *Gt) IsEqual(x *Gt) bool   { return (*ff.Fp12)(z).IsEqual((*ff.Fp12)(x)) }
 func (z *Gt) Exp(x *Gt, n *Scalar) { (*ff.Fp12)(z).Exp((*ff.Fp12)(x), n.Bytes()) }
 func (z *Gt) Mul(x, y *Gt)         { (*ff.Fp12)(z).Mul((*ff.Fp12)(x), (*ff.Fp12)(y)) }
+func (z *Gt) Sqr(x *Gt)            { (*ff.Fp12)(z).Sqr((*ff.Fp12)(x)) }
+func (z *Gt) Inv(x *Gt)            { (*ff.Fp12)(z).Inv((*ff.Fp12)(x)) }
