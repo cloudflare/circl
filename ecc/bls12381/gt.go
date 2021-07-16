6deaab7fab20165930cd180f7191ed638bf64083
Armando Faz <armfazh@users.noreply.github.com>
2021-07-15 18:48:20 -0700

    Adding prodPairing test. (#246)

--- ecc/bls12381/gt.go
+++ ecc/bls12381/gt.go
@@ -1,7 +1,10 @@
 package bls12381
 
-import (
-	"github.com/cloudflare/circl/ecc/bls12381/ff"
-)
+import "github.com/cloudflare/circl/ecc/bls12381/ff"
 
 type Gt ff.Fp12
+
+func (z *Gt) SetOne()              { (*ff.Fp12)(z).SetOne() }
+func (z *Gt) IsEqual(x *Gt) bool   { return (*ff.Fp12)(z).IsEqual((*ff.Fp12)(x)) }
+func (z *Gt) Exp(x *Gt, n *Scalar) { (*ff.Fp12)(z).Exp((*ff.Fp12)(x), n.Bytes()) }
+func (z *Gt) Mul(x, y *Gt)         { (*ff.Fp12)(z).Mul((*ff.Fp12)(x), (*ff.Fp12)(y)) }
