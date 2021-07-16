342525e62b81596277a999ad58dbe013a0625042
armfazh <armfazh@cloudflare.com>
2021-07-15 18:03:57 -0700

    Adding prodPairing test.

--- ecc/bls12381/pair_test.go
+++ ecc/bls12381/pair_test.go
@@ -5,7 +5,38 @@ import (
 	"testing"
 
 	"github.com/cloudflare/circl/ecc/bls12381/ff"
+	"github.com/cloudflare/circl/internal/test"
 )
+
+func TestProdPair(t *testing.T) {
+	const testTimes = 1 << 5
+	const N = 3
+
+	listG1 := [N]*G1{}
+	listG2 := [N]*G2{}
+	listSc := [N]*Scalar{}
+	var ePQn, got Gt
+
+	for i := 0; i < testTimes; i++ {
+		got.SetOne()
+		for j := 0; j < N; j++ {
+			listG1[j] = randomG1(t)
+			listG2[j] = randomG2(t)
+			listSc[j] = &Scalar{}
+			listSc[j].Random()
+
+			ePQ := Pair(listG1[j], listG2[j])
+			ePQn.Exp(ePQ, listSc[j])
+			got.Mul(&got, &ePQn)
+		}
+
+		want := ProdPair(listG1[:], listG2[:], listSc[:])
+
+		if !got.IsEqual(want) {
+			test.ReportError(t, got, want)
+		}
+	}
+}
 
 func BenchmarkMiller(b *testing.B) {
 	g1 := G1Generator()
@@ -56,12 +87,12 @@ func BenchmarkPair(b *testing.B) {
 		listExp[i].Random()
 	}
 
-	b.Run("Pair1", func(b *testing.B) {
+	b.Run("Pair", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			Pair(g1, g2)
 		}
 	})
-	b.Run(fmt.Sprintf("Pair%v", N), func(b *testing.B) {
+	b.Run(fmt.Sprintf("ProdPair%v", N), func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			ProdPair(listG1[:], listG2[:], listExp[:])
 		}
