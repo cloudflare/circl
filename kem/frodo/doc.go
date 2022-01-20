651d2a17341d837d597081b1f1e063632b678dd7
xvzcf <xvzcf@users.noreply.github.com>
2022-01-20 15:43:27 -0500

    Added implementation of FrodoKEM-640-SHAKE-CCA.

---
+++ kem/frodo/doc.go
@@ -0,0 +1,8 @@
+// Package frodo implements the FrodoKEM IND-CCA2 secure
+// key encapsulation mechanism (KEM) as submitted to round 3 of the NIST PQC
+// competition and described in
+//
+// https://frodokem.org/files/FrodoKEM-specification-20210604.pdf
+// This implementation draws heavily from the PQClean implementation found at
+// https://github.com/PQClean/PQClean/tree/master/crypto_kem/frodokem640shake/clean
+package frodo
