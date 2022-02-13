d0778d46ef36cbd6100fd5251bf8a336ba460c10
Goutam Tamvada <xvzcf@users.noreply.github.com>
2022-02-13 10:10:57 +0100

    Update kem/frodo/doc.go
    Co-authored-by: Armando Faz <armfazh@users.noreply.github.com>

--- kem/frodo/doc.go
+++ kem/frodo/doc.go
@@ -1,8 +1,10 @@
-// Package frodo implements the FrodoKEM IND-CCA2 secure
-// key encapsulation mechanism (KEM) as submitted to round 3 of the NIST PQC
-// competition and described in
+// Package frodo provides the key encapsulation mechanism FrodoKEM.
 //
-// https://frodokem.org/files/FrodoKEM-specification-20210604.pdf
-// This implementation draws heavily from the PQClean implementation found at
-// https://github.com/PQClean/PQClean/tree/master/crypto_kem/frodokem640shake/clean
+// Compatible with the implementation submitted to round 3 of the
+// NIST PQC competition [1]. This implementation draws heavily from the PQClean
+// implementation [2].
+//
+// References:
+//  [1] https://frodokem.org/files/FrodoKEM-specification-20210604.pdf
+//  [2] https://github.com/PQClean/PQClean/tree/master/crypto_kem/frodokem640shake/clean
 package frodo
