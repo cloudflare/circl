058ec245ce78bc1a9e70339f4a4b870b51561e73
armfazh <armfazh@cloudflare.com>
2022-05-02 21:18:25 -0700

    Remove unused functions in rsablind.

--- blindsign/blindrsa/rsa.go
+++ blindsign/blindrsa/rsa.go
@@ -42,10 +42,6 @@ var (
 	bigOne  = big.NewInt(1)
 )
 
-// ErrVerification represents a failure to verify a signature.
-// It is deliberately vague to avoid adaptive attacks.
-var ErrVerification = errors.New("crypto/rsa: verification error")
-
 // incCounter increments a four byte, big-endian counter.
 func incCounter(c *[4]byte) {
 	if c[3]++; c[3] != 0 {
@@ -164,7 +160,7 @@ func decrypt(random io.Reader, priv *rsa.PrivateKey, c *big.Int) (m *big.Int, er
 		m.Mod(m, priv.N)
 	}
 
-	return m, err
+	return m, nil
 }
 
 func decryptAndCheck(random io.Reader, priv *rsa.PrivateKey, c *big.Int) (m *big.Int, err error) {
