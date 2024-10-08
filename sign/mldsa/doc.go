016979235dbea073bbe844fe8766a0f63cbf061c
Bas Westerbaan <bas@westerbaan.name>
2024-10-08 14:52:51 +0200

    Update ml-dsa documentation

--- sign/mldsa/doc.go
+++ sign/mldsa/doc.go
@@ -1,2 +1,12 @@
-// mldsa implements NIST post-quantum signature scheme ML-DSA.
+// mldsa implements NIST post-quantum signature scheme ML-DSA (FIPS204)
+//
+// Each of the three different security levels of ML-DSA is implemented by a
+// subpackage. For instance, mldsa44 can be found in
+//
+//	github.com/cloudflare/circl/sign/mldsa/mldsa44
+//
+// If your choice for mode is fixed compile-time, use the subpackages.
+// To choose a scheme at runtime, use the generic signatures API under
+//
+//	github.com/cloudflare/circl/sign/schemes
 package mldsa
