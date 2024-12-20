662cdff126a3628f858c01b02b5b1ab520cbc27d
armfazh <armfazh@cloudflare.com>
2024-12-20 12:59:57 -0600

    Add documentation to vdaf package.

--- vdaf/doc.go
+++ vdaf/doc.go
@@ -1,2 +1,5 @@
 // Package vdaf provides verifiable distributed aggregation functions.
+//
+// This package supports Prio3 as specified in [v13].
+// [v13]: https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-vdaf-13
 package vdaf
