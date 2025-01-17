3d8cc9889057c81b9a9ed52ff18f7025e64ee961
armfazh <armfazh@cloudflare.com>
2025-01-17 14:18:36 -0800

    Bump to v14.

--- vdaf/prio3/doc.go
+++ vdaf/prio3/doc.go
@@ -1,11 +1,11 @@
 // Package prio3 implements Prio3, a set of Verifiable Distributed
 // Aggregation Functions (VDAFs). It provides several concrete VDAFs
-// described in draft-irtf-cfrg-vdaf [v13]:
+// described in draft-irtf-cfrg-vdaf [v14]:
 //   - Count [github.com/cloudflare/circl/vdaf/prio3/count]
 //   - Sum [github.com/cloudflare/circl/vdaf/prio3/sum]
 //   - SumVec [github.com/cloudflare/circl/vdaf/prio3/sumvec]
 //   - Histogram [github.com/cloudflare/circl/vdaf/prio3/mhcv]
 //   - MultiHotCountVec [github.com/cloudflare/circl/vdaf/prio3/histogram]
 //
-// [v13]: https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-vdaf-13
+// [v14]: https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-vdaf-14
 package prio3
