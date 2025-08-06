ac4929740e4d95472de158bb05b4de00e0c42125
Bas Westerbaan <bas@cloudflare.com>
2025-08-06 13:34:51 +0200

    pki: add ML-DSA private key tests

--- pki/pki_test.go
+++ pki/pki_test.go
@@ -1,9 +1,14 @@
 package pki_test
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
 	"testing"
 
+	"github.com/cloudflare/circl/internal/test"
 	"github.com/cloudflare/circl/pki"
+	"github.com/cloudflare/circl/sign"
 	"github.com/cloudflare/circl/sign/schemes"
 )
 
@@ -52,4 +57,59 @@ func TestPEM(t *testing.T) {
 			}
 		})
 	}
+}
+
+func testMLDSAHappy(t *testing.T, fn string) {
+	t.Run(fn+" happy", func(t *testing.T) {
+		err := testMLDSA(t, fn)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+}
+
+func testMLDSASad(t *testing.T, fn string) {
+	t.Run(fn+" sad", func(t *testing.T) {
+		err := testMLDSA(t, fn)
+		if err == nil {
+			t.Fatal("Expected error")
+		}
+	})
+}
+
+func testMLDSA(t *testing.T, fn string) error {
+	pem, err := test.ReadGzip("testdata/" + fn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sk, err := pki.UnmarshalPEMPrivateKey(pem)
+	if err != nil {
+		return err
+	}
+
+	seed := sk.(sign.Seeded).Seed()
+	if seed == nil {
+		return errors.New("seed not retained")
+	}
+
+	if !bytes.Equal(seed, []byte{
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+		19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+	}) {
+		return errors.New("unexpected seed")
+	}
+
+	return nil
+}
+
+func TestMLDSA(t *testing.T) {
+	// Tests taken from draft-ietf-lamps-dilithium-certificates-12
+	for _, lvl := range []string{"44", "65", "87"} {
+		for _, tpe := range []string{"seed", "both"} {
+			testMLDSAHappy(t, fmt.Sprintf("ML-DSA-%s-%s.priv.gz", lvl, tpe))
+		}
+		testMLDSASad(t, fmt.Sprintf("ML-DSA-%s-expanded.priv.gz", lvl))
+	}
+
+	testMLDSASad(t, "bad-ML-DSA-44-1.priv.gz")
 }
