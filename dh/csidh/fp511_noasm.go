c99f87b50e8bba766040a1e80be23ce7765fe46a
Bas Westerbaan <bas@cloudflare.com>
2026-07-14 19:06:48 +0200

    mul576: remove dead code

--- dh/csidh/fp511_noasm.go
+++ dh/csidh/fp511_noasm.go
@@ -3,7 +3,6 @@
 
 package csidh
 
-func mul512(r, m1 *fp, m2 uint64)            { mul512Generic(r, m1, m2) }
-func mul576(r *[9]uint64, m1 *fp, m2 uint64) { mul576Generic(r, m1, m2) }
-func cswap512(x, y *fp, choice uint8)        { cswap512Generic(x, y, choice) }
-func mulRdc(r, x, y *fp)                     { mulRdcGeneric(r, x, y) }
+func mul512(r, m1 *fp, m2 uint64)     { mul512Generic(r, m1, m2) }
+func cswap512(x, y *fp, choice uint8) { cswap512Generic(x, y, choice) }
+func mulRdc(r, x, y *fp)              { mulRdcGeneric(r, x, y) }
